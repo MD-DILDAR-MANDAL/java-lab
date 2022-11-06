4b6873ed9df40ce644a6aeb1d60977fa6a35953f
MD DILDAR MANDAL <dildarmandal335@gmail.com>
2022-11-06 23:50:48 +0530

    written

--- java lab/patrn_17.java
+++ java lab/patrn_17.java
@@ -1,12 +1,22 @@
-import java.lang.*;
-
 class patrn_17{
 	public static void main(String[] args){
-		for(int row = 1;row <= 8;row++){
-			for(int space = 0;space < 8 - row;space++){
-
+		int n=8,temp=2;
+			for(int i=1 = 0;i <= n;i++){
+				for(int j=1;j<=(n-i);j++){
+					System.out.print(" ");
+				}
 			}
-		}
+			for(int j=1;j<=(2*(i-1))++1;j++){
+				if(j<=i){
+					System.out.print(j);
+				}
+				else{
+					System.out.print(j-temp);
+					temp+=2;
+				}
+			}
+		temp = 2;
+		System.out.print("\n");
 		
     }
 }
