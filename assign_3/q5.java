3ceab87e4f386320278d3c94a414862ab1de8725
MD-DILDAR-MANDAL <dildarmandal335@gmail.com>
2022-11-18 23:27:50 +0530

    q5 finally done after 3 days of stress <3

--- assign_3/q5.java
+++ assign_3/q5.java
@@ -1,6 +1,7 @@
+
 import java.util.*;
 
-class q5{
+class Main{
 	public static void main(String args[]){
 	
 		Scanner sc = new Scanner(System.in);
@@ -11,26 +12,25 @@ class q5{
 		cntArrElm = new int[10];
 		
 		for(int i =0;i<10;i++) cntArrElm[i]=0;
-	
-		for(int i = 0;i<size;i++){
-			arr[i] = sc.nextInt();
-		}	
+		for(int i = 0;i<size;i++) arr[i] = sc.nextInt();
 		for(int i = 0;i<size;i++) cntArrElm[arr[i]]++;
-		int len = arr.length;
-		int sizeC= size;
+    
+		int sizec= size;
+    
 		for(int i=size-1;i>=0;i--){
 			if(cntArrElm[arr[i]]>1){
-				if(i!=size-1){
-					arr[i]=0;
-					for(int j = i;j<size-1;j++){
-						arr[j]=arr[j+1];
+        if(i == size-1){
+          sizec--;
+          cntArrElm[arr[i]] --;
+        }
+        else{
+					cntArrElm[arr[i]] --;	
+          sizec--;
+          for(int j = i;j<(size-1);j++)
+          arr[j]=arr[j+1];
 					}
 				}
-				cntArrElm[arr[i]] --;
-				sizeC--;
-		System.out.println(Arrays.toString(arr).substring(0,sizeC));
-			}
 		}
-		System.out.println(Arrays.toString(arr));
+    for(int i = 0;i<sizec;i++)System.out.print(arr[i]+ " ");
 	}
 }
