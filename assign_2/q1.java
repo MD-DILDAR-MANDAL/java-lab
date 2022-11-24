fc1f9490a43789c8581a9606d90d972a9c5828d5
MD-DILDAR-MANDAL <dildarmandal335@gmail.com>
2022-11-24 19:02:13 +0530

    assign 2 ho gya chor abb

--- assign_2/q1.java
+++ assign_2/q1.java
@@ -1,56 +1,122 @@
 import java.util.*;
 
-class details{
-			string name;
-			string gender;
-			int age;
-			string seatype;
-			public void details(){
-				System.out.println("enter name: ");
-				this.name=sc.nextLine();
-				System.out.println("enter gender...male/female ");
-				this.gender=sc.nextLine();
-				System.out.println("enter age: ");
-				this.age=sc.nextInt();
-				System.out.println("seatype...low,up,mid,sidelow,sideup");
-				this.seatype=sc.nextLine();
-			}
-			public void display(){
-				System.out.println("name: "+ name);
-				System.out.println("gender: "+ gender);
-				System.out.println("age: "+age);
-				System.out.println("seatype"+seatype);
-			}
+class passengers{
+	String name;
+	int age;
+	String seatchoice;
+	public void passengers(){
+		System.out.println("Enter the name: ");
+		this.name=scn.nextLine();
+		System.out.println("Enter the age: ");
+		this.age=scn.nextInt();
+		System.out.println("Enter the Seat choice: ");
+		System.out.println("low, mid, up, upside, lowside");
+		this.seatchoice=nextLine();
+	}
 }
 
-class q1{
-	Scanner sc=new Scanner(System.in);
-	public static void main(String args[]){
-		char ans="y";
-		int low=0,up=0,mid=0,sidelow=0,sideup=0;
-		while(ans == "y"){
-			System.out.println("enter no. of people for reservation");
-			int nop = sc.nextInt();
-			
-			details arr[];
-			arr = new details[nop];
-			
-			for(int i=0;i<nop;i++){
-				arr[i].details();
+class reservation{
+	String pname;
+	String seatalloted;
+	public static reservation(String namep,String pseatallocated){
+		this.pname=namep;
+		this.seatalloted=pseatalloted;}
+	void display(){
+		System.out.println("Passenger name: " + name );
+		System.out.println("Seat alloted: " + seatalloted);
+	}
+}
 
-			}
-			
-			
-			//if 3 seats per then 1 seat for aged 
-			//aged cnt++ with seat choice :condition:
-			if(seatype == "low" && low)
-			for(int i=0;i<nop;i++){
-				arr[i].display();
-			}
-			
-			
-			System.out.println("want to enter again ...y/n");
-			ans=sc.next().CharAt(0);
+class as2q1{
+	static void allocate(){
+		switch((arr[i].seatchoice)){
+			case "low":
+				if (low>0){
+					System.out.println("Seat booked");
+					low--;
+					arrr[i].reservation(name,"low");}
+				else{
+					System.out.println("Seat filled up");
+					System.out.println("Alloting to other empty seats");
+					otherseat((arr[i].seatchoice));}
+			case "mid":
+				if (mid>0){
+					System.out.println("Seat booked");
+					mid--;
+					arrr[i].reservation(name,"mid");}
+				else{
+					System.out.println("Seat filled up");
+					System.out.println("Alloting to other empty seats");
+					otherseat((arr[i].seatchoice));}
+			case "up":
+				if (up>0){
+					System.out.println("Seat booked");
+					up--;
+					arrr[i].reservation(name,"up");}
+				else{
+					System.out.println("Seat filled up");
+					System.out.println("Alloting to other empty seats");
+					otherseat((arr[i].seatchoice));}
+			case "upside":
+				if (upside>0){
+					System.out.println("Seat booked");
+					upside--;
+					arrr[i].reservation(name,"upside");}
+				else{
+					System.out.println("Seat filled up");
+					System.out.println("Alloting to other empty seats");
+					otherseat((arr[i].seatchoice));}
+			case "lowside":
+				if (lowside>0){
+					System.out.println("Seat booked");
+					lowside--;
+					arrr[i].reservation(name,"lowside");}
+				else{
+					System.out.println("Seat filled up");
+					System.out.println("Alloting to other empty seats");
+					otherseat((arr[i].seatchoice));}
+	}
+}
+	public static void otherseat((arr[i].seatchoice)){
+		if (low>0)
+			arrr[i].reservation(name,"low");
+		else if (mid>0)
+			arrr[i].reservation(name,"mid");
+		else if (up>0)
+			arrr[i].reservation(name,"up");
+		else if (upside>0)
+			arrr[i].reservation(name,"upside");
+		else if (lowside>0)
+			arrr[i].reservation(name,"lowside");
+	}
+	public static void main(String args[]){
+		Scanner scn=new Scanner(System.in);
+		//Program is just limited for 24 seats as per the constraints set
+		System.out.println("Total seats left is 24");
+		System.out.println("The Seats are reserved for female");
+		int upside=3,lowside=3,up=6,mid=6,low=6,n=24;
+		//Array initialisation for Passenger class
+		passengers arr[];
+		arr = new details[24];
+		//Array initialisation for reservation class
+		reservation arrr[];
+		arrr=new reservation[24];
+		for (int i=0;i<24;++i)	arr[i].passsengers();
+		//Alloting seats to senior citizen
+		for (int i=0;i<24;i++){
+			if (arr[i].age>=65)	allocate();
+		}
+		//To allocate the seats to passenger below 65 years
+		for (int i=0;i<24;i++){
+			if (arr[i].age<65)	allocate();
+		}		 
+		//Printing the allocated seats
+		for (int i=0;i<24;i++){
+			arrr[i].display();
+			System.out.println();
 		}
 	}
 }
+		
+			
+
