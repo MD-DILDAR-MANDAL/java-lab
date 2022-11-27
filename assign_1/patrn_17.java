ecbddbd086245e566369f569c730812ec2cd08b2
MD-DILDAR-MANDAL <dildarmandal335@gmail.com>
2022-11-28 00:35:05 +0530

    corrected

--- assign_1/patrn_17.java
+++ assign_1/patrn_17.java
@@ -1,22 +1,21 @@
 class patrn_17{
 	public static void main(String[] args){
-		int n=8,temp=2;
-			for(int i=1 = 0;i <= n;i++){
-				for(int j=1;j<=(n-i);j++){
+	
+			for(int i=1;i <= 8;i++){
+				for(int j=1;j<=(8-i);j++){
 					System.out.print(" ");
 				}
-			}
-			for(int j=1;j<=(2*(i-1))++1;j++){
-				if(j<=i){
-					System.out.print(j);
+				int temp = 2;
+				for(int j=1;j<=2*i -1;j++){
+					if(j<=i){
+						System.out.print(j);
+					}
+					else{
+						System.out.print(j-temp);
+						temp+=2;
+					}
 				}
-				else{
-					System.out.print(j-temp);
-					temp+=2;
-				}
-			}
-		temp = 2;
-		System.out.print("\n");
-		
-    }
+				System.out.print("\n");
+			}	
+	}
 }
--- assign_1/patrn_18.java
+++ assign_1/patrn_18.java
@@ -1,20 +1,21 @@
 class patrn_18{
-  public static void main(String args[]){
-    int n = 8,temp = 2;
-    for(int i=1;i<=n;i++){
-      for(int j=1;j<=i-1;j++){
-        System.out.print(" ");
-      }
-      for(inti;j <=(2*(n-i))+1;i++){
-        if(j<=(n-(i-1))){
-          System.out.print(j);
-        }
-        else System.out.print(i-temp);
-        temp += 2;
-      }
-      temp =2;
-      System.out.print("\n";)
-   }
-    
- }
+	public static void main(String args[]){
+		for(int i=8;i>=1;i--){
+		
+      			for(int j=8-i;j>=0;j--){
+        			System.out.print(" ");
+      			}
+      			int temp = 2;
+      			for(int j =1;j <=2*i-1;j++){
+        			if(j<=i){
+          				System.out.print(j);
+        			}	
+        			else{
+        			 	System.out.print(j-temp);
+        				temp += 2;
+      				}
+   			}
+   			System.out.print("\n");
+ 		}
+	}
 }
